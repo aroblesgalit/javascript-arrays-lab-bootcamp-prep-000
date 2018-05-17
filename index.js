882bb4d0cb9f1b1a3c787ff61d9a54437c931dd2
Alvin Galit <aroblesgalit@gmail.com>
2018-05-17 01:54:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+}
+
+function appendKitten(name) {
+  return [...kittens, name];
+}
+
+function prependKitten(name) {
+  return [name, ...kittens];
+}
+
+function removeLastKitten() {
+  kittens.splice(-1, 1);
+  return kittens;
+}
+
+function removeFirstKitten() {
+  kittens.splice(0, 1);
+  return kittens;
+}
